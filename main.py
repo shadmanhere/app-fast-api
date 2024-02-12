ea276e692e167b7a14f9f84ee82ecd7eff242b66
Shadman Ali <shadman.ali@live.co.uk>
2024-02-12 11:52:13 +0530

    add all routes/api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from item import Item
 
 # Create an instance of the FastAPI class
 app = FastAPI()
@@ -6,9 +7,31 @@ app = FastAPI()
 # Define a route
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, World"}
+    return {"message": "Hello!, World"}
+
+# GET request
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    if item_id == 42:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return {"item_id": item_id}
+
+# POST request
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+# PUT request
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+# DELETE request
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"item_id": item_id, "deleted": True}
 
 # Run the server
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="localhost", port=8000)
